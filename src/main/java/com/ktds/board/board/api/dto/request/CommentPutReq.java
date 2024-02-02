2b5f030413ef2a694355d5320689188cf73c9c73
이방환 <soulb9@naver.com>
2024-02-02 17:20:39 +0900

    feat: validation 어노테이션 추가

--- src/main/java/com/ktds/board/board/api/dto/request/CommentPutReq.java
+++ src/main/java/com/ktds/board/board/api/dto/request/CommentPutReq.java
@@ -1,7 +1,17 @@
 package com.ktds.board.board.api.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+
 public record CommentPutReq(
+	@Schema(description = "댓긓 식별자", type = "Long", example = "1")
+	@Positive(message = "필수 입력항목입니다. (양수)")
+	@Max(value = Long.MAX_VALUE, message = "id 범위를 벗어났습니다.")
 	Long id,
+
+	@Schema(description = "변경한 댓글 내용", type = "String", example = "댓글 내용1")
+	@Positive(message = "필수 입력항목입니다. (String)")
 	String content
 ) {
 }
--- src/main/java/com/ktds/board/board/api/dto/request/LikePostReq.java
+++ src/main/java/com/ktds/board/board/api/dto/request/LikePostReq.java
@@ -1,12 +1,32 @@
 package com.ktds.board.board.api.dto.request;
 
+import com.ktds.board.board.db.entity.enumtype.LikeContentType;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 
 public record LikePostReq (
+
+	@Schema(description = "좋아요 식별자", type = "Long", example = "1")
+	@Positive(message = "필수 입력항목입니다. (양수)")
+	@Max(value = Long.MAX_VALUE, message = "id 범위를 벗어났습니다.")
 	Long id,
+
+	@Schema(description = "좋아요 식별자", type = "Long", example = "1")
+	@Positive(message = "필수 입력항목입니다. (양수)")
+	@Max(value = Long.MAX_VALUE, message = "id 범위를 벗어났습니다.")
 	Long userId,
+
+	@Schema(description = "좋아요 식별자", type = "Long", example = "1")
+	@Positive(message = "필수 입력항목입니다. (양수)")
+	@Max(value = Long.MAX_VALUE, message = "id 범위를 벗어났습니다.")
 	Long articleId,
-	String type
+
+	@Schema(description = "좋아요한 컨텐츠 유형", type = "Enum", example = "ARTICLE")
+	@Positive(message = "필수 입력항목입니다. (String)")
+	LikeContentType type
 ) {
 }
