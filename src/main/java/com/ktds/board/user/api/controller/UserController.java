cc961987decf7fc2fc748965ce33654b14a31407
이방환 <soulb9@naver.com>
2024-02-05 17:50:10 +0900

    refactor: 리팩토링

--- src/main/java/com/ktds/board/user/api/controller/UserController.java
+++ src/main/java/com/ktds/board/user/api/controller/UserController.java
@@ -1,24 +1,28 @@
 package com.ktds.board.user.api.controller;
 
-import com.ktds.board.common.annotation.ApiDocumentResponse;
-import com.ktds.board.common.entity.BaseResponseBody;
-import com.ktds.board.user.api.dto.request.UserGetReq;
-import com.ktds.board.user.api.dto.request.UserPostReq;
-import com.ktds.board.user.api.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import lombok.RequiredArgsConstructor;
+import java.net.URI;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
+import com.ktds.board.common.annotation.ApiDocumentResponse;
+import com.ktds.board.common.entity.BaseResponseBody;
+import com.ktds.board.user.api.dto.request.UserEmailExistsReq;
+import com.ktds.board.user.api.dto.request.UserGetReq;
+import com.ktds.board.user.api.dto.request.UserNicknameExistsReq;
+import com.ktds.board.user.api.dto.request.UserPostReq;
+import com.ktds.board.user.api.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Tag(name = "사용자 관리 API", description = "사용자 정보를 조회, 추가, 수정, 삭제하는 API입니다.")
 @Validated
@@ -31,7 +35,7 @@ public class UserController {
 
 	@ApiDocumentResponse
 	@Operation(summary = "getUser", description="사용자 정보 조회")
-	@GetMapping
+	@PostMapping("/user")
 	public ResponseEntity<? extends BaseResponseBody> getUser(
 		@RequestBody @Valid UserGetReq req) {
 		var user = userService.getOne(req);
@@ -59,14 +63,11 @@ public class UserController {
 
 	@ApiDocumentResponse
 	@Operation(summary = "isUserEmailExists", description="이메일 중복체크")
-	@GetMapping("/exists/email/{email}")
+	@PostMapping("/exists/email")
 	public ResponseEntity<? extends BaseResponseBody> isUserEmailExists(
-			@PathVariable("email")
-			@NotBlank(message = "필수 입력 항목입니다.")
-			@Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$", message = "이메일 형식에 맞지 않습니다.")
-			String email) {
+		@RequestBody @Valid UserEmailExistsReq req) {
 
-		var exists =  userService.emailExists(email);
+		var exists =  userService.emailExists(req.email());
 
 		return ResponseEntity
 			.ok()
@@ -75,18 +76,15 @@ public class UserController {
 
 	@ApiDocumentResponse
 	@Operation(summary = "isUserNicknameExists", description="닉네임 중복체크")
-	@GetMapping("/exists/nickname/{nickname}")
+	@PostMapping("/exists/nickname")
 	public ResponseEntity<? extends BaseResponseBody> isUserNicknameExists(
-			@Schema(description = "닉네임", type = "String", example = "nickname1")
-			@NotBlank(message = "필수 입력 항목입니다.")
-			@PathVariable("nickname") String nickname) {
+			@RequestBody @Valid UserNicknameExistsReq req) {
 
-		var exists =  userService.nicknameExists(nickname);
+		var exists =  userService.nicknameExists(req.nickname());
 
 		return ResponseEntity
 				.ok()
 				.body(new BaseResponseBody<>(HttpStatus.OK, exists));
 	}
-
 
 }
