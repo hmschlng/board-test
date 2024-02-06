aaf010a59f96b78d8d95b1bd5629c24d573e36e0
이방환 <soulb9@naver.com>
2024-02-06 16:32:31 +0900

    refactor: 사용자 엔티티에서 name 삭제
    사용자의 실명이 서비스에 쓰일 일이 없을 것으로 예상하여 name column을 삭제했습니다.

--- src/main/java/com/ktds/board/user/db/entity/User.java
+++ src/main/java/com/ktds/board/user/db/entity/User.java
@@ -1,6 +1,6 @@
 package com.ktds.board.user.db.entity;
 
-import com.ktds.board.common.entity.BaseTimeEntity;
+import com.ktds.board.common.audit.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -17,17 +17,12 @@ public class User extends BaseTimeEntity {
 
     @Id
     @Column(name = "user_id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "필수 입력 항목입니다.")
     @Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$", message = "이메일 형식에 맞지 않습니다.")
     @Column(name = "email_id", nullable = false)
     private String email;
-
-    @NotBlank(message = "필수 입력 항목입니다.")
-    @Column(name = "name", nullable = false)
-    private String name;
 
     @Column(name = "nickname", nullable = false)
     private String nickname;
